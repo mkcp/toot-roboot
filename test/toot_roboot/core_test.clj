6c2360912a96a588bc69c7334cd83f8f3cdb534e
Michaela Patella <m.c.patella@gmail.com>
2015-12-14 08:04:57 -0500

    add truncated markov tree for testing purposes and begin testing

--- test/toot_roboot/core_test.clj
+++ test/toot_roboot/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [toot-roboot.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-markov-tree
+  (-> "/test/test_data/markov-data.edn"
+      slurp
+      read))
+
+(deftest tweeting-test
+  (testing "tweet generation"
+    (let [tweet (make-tweet test-markov-data)]
+      (is (<= (count (seq tweet))
+              140)))))
